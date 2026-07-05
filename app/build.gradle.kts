9ad408b7466cfb73ba8a67f9f9d4ecd3533f41ad
jrs8205 <jarsi@jarsi.org>
2026-07-05 14:55:38 +0300

    Bump version to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.example.modernwidget"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.2.1"
+        versionCode = 13
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
